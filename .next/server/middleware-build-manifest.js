8291945ad5cf3e1f1212f128f6777735b0c488e5
jcason21 <jcasondev@gmail.com>
2025-06-06 12:06:47 -0400

    Update commit

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -9,6 +9,11 @@ self.__BUILD_MANIFEST = {
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "pages": {
+    "/": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/index.js"
+    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -18,16 +23,6 @@ self.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
-    ],
-    "/liondesk": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/liondesk.js"
-    ],
-    "/shyndig": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/shyndig.js"
     ],
     "/studio": [
       "static/chunks/webpack.js",
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "72c11c08994ff6a5"; }
+/******/ 		__webpack_require__.h = function() { return "c8e78d777ed561a0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
